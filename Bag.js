0c1b4c80bf386635d7ae7b589217efae6cdb2807
phillipsalangsang <phillip_salangsang@hotmail.com>
2023-02-12 19:52:51 +0000

    max weight 23

--- Bag.js
+++ Bag.js
@@ -1,7 +1,18 @@
+// class Bag {
+//     constructor(weight) {
+//         if(!weight){
+//             throw new Error('bag needs weight')
+//         } 
+//         this.weight = weight
+//     }
+// }
+
 class Bag {
+    static MAX_WEIGHT = 23;
+
     constructor(weight) {
-        if(!weight){
-            throw new Error('bag needs weight')
+        if(weight > Bag.MAX_WEIGHT) {
+            throw new Error(`Bag weight can not be greater than ${Bag.MAX_WEIGHT} kg`)
         } 
         this.weight = weight
     }
